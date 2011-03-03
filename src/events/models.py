d9d9580528729a005e62f4866c985c8870fe021b
Alfredo Aguirre <alfredoaguirre@theteam.co.uk>
2011-03-03 14:33:06 +0000

    Simple model for the events and view to display  them

--- src/events/models.py
+++ src/events/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+from djqmgr import QManager
+from django_extensions.db.fields import CreationDateTimeField,\
+     ModificationDateTimeField, AutoSlugField
+
+class Event(models.Model):
+    """Events"""
+    LIVE = 1
+    HIDDEN = 2
+    REMOVED = 3
+    STATUS_CHOICES = ((LIVE, _('Live')),
+                      (HIDDEN, _('Hidden')),
+                      (REMOVED, _('Removed')),
+                      )
+    title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='title')
+    summary = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+    image = models.ImageField(upload_to="event", blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
+    location = models.CharField(blank=True, max_length=255)
+    created = CreationDateTimeField()
+    modified = ModificationDateTimeField()
+    status = models.IntegerField(choices=STATUS_CHOICES,
+                                 default=LIVE)
+    is_featured = models.BooleanField(default=False)
+    url = models.URLField(verify_exists=False, blank=True)
+
+    # managers
+    objects = models.Manager()
+    live = QManager(status=LIVE)
+
+    def __unicode__(self):
+        return u'Event: %s ' % self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('events:event_detail', [self.slug,])
--- src/events/views.py
+++ src/events/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+
+from events.models import Event
+
+def object_list(request, template='events/object_list.html'):
+    """List of the ``LIVE`` ``Event`` ordered by Featured and Date"""
+    object_list = Event.live.all().order_by('is_featured','-created')
+    extra_context = {'object_list': object_list,}
+    return render_to_response(template, extra_context,
+                              RequestContext(request))
+
+def object_detail(request, slug, template='events/object_detail.html'):
+    """Shows the requested ``EVENT`` if ``LIVE``"""
+    event = get_object_or_404(Event, slug=slug, status=Event.LIVE)
+    extra_context = {'object': event}
+    return render_to_response(template, extra_context,
+                              RequestContext(request))
